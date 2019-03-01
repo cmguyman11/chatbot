8a947e48394548e3896371a9967323e3a7503991
cmguyman11 <cmguyman11@gmail.com>
2019-03-01 13:27:50 -0800

    temp fix, doesnt' find harry potter but doesn't find too many for te

--- chatbot.py
+++ chatbot.py
@@ -478,8 +478,8 @@ class Chatbot:
         movie_noalt = re.sub(' \(.*\)', '', movie)
         if title in [movie, movie_with_year, movie_noalt] and not self.creative:
           id_list.append(i)
-        if self.creative and title in movie and not found:
-          id_list.append(i)
+        # if self.creative and title in movie and not found:
+        #   id_list.append(i)
             
       id_list = list(set(id_list))
       return id_list
